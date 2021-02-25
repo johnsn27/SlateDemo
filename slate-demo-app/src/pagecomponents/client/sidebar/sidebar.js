6336aa65b0a40193ed7bb5f5c25b04c8660cc8c3
johnsn27 <nathan.johnson@bbc.co.uk>
2021-02-25 17:20:24 +0000

    Add Sidebar close button

--- slate-demo-app/src/pagecomponents/client/sidebar/sidebar.js
+++ slate-demo-app/src/pagecomponents/client/sidebar/sidebar.js
@@ -4,6 +4,16 @@ import Btn from '@bbc/igm-btn';
 import GELIcon from '@bbc/igm-gel-icon';
 import './scss/sidebar.scss';
 
+const noIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 13 13"
+    focusable="false"
+  >
+    <path d="M8.63,6.5,13,10.87,10.87,13,6.5,8.63,2.13,13,0,10.87,4.37,6.5,0,2.13,2.14,0,6.5,4.37,10.87,0,13,2.13Z" />
+  </svg>
+)
+
 function Sidebar(props) {
   const { editorFocused, children, toggle } = props;
 
@@ -13,7 +23,7 @@ function Sidebar(props) {
         <div className="sidebar__context-bar" />
       )}
       <Btn onClick={() => toggle(false)} className="sidebar__close" aria-label="Close shortcuts sidebar">
-        <GELIcon type="no" className="sidebar__close-icon" />
+        <GELIcon customSvg={noIcon} className="sidebar__close-icon"/>
       </Btn>
       <div className="sidebar__content">{children}</div>
     </div>
